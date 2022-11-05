7186b2e6a93f74b5e613027a50099c0f60c10de0
Mike Peiman <mikepeiman@gmail.com>
2022-11-05 19:50:13 -0400

    Cloned/installed https://github.com/mattjennings/sveltekit-blog-template and updated basic personal info

---
+++ src/lib/info.js
@@ -0,0 +1,26 @@
+// This is for convenience as you start out with this template.
+// If you'd rather hardcode these values into your pages,
+// feel free to remove this file.
+
+// URL of your website
+export const website = 'https://svexblog.vercel.app/'
+
+export const firstName = 'Mike'
+export const lastName = 'Peiman'
+export const name = `${firstName} ${lastName}`
+
+// Used for the landing page & footer of posts
+// export const avatar = 'mike_profile_headshot 2 20220827_135617.jpg'
+export const avatar = 'mikepeiman_a_realistic_large_proud_and_fierce_lion_with_a_mane__df36fb93-d7d3-448f-8fe5-5b3b67fb69da.png'
+export const bio = `\
+I’m ${firstName}, a self-taught web & software developer based in Canada. I'm passionate about UI & UX, tools for thought, and improving the human condition in every way possible. I have a background in health, fitness and snowboarding, as well as in sustainability.
+I'm also a solo father, raising and homeschooling my daughter on my own. 
+`
+
+// Your social usernames, empty strings won't be used.
+// For sake of the live demo, I'm using `username/repo` for github,
+// but you can replace with just your username.
+export const github = 'mikepeiman'
+export const twitter = ''
+export const linkedin = ''
+export const instagram = ''
